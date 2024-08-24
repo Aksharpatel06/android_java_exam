a45a49e3d150aef24589008ccdc6a2c25e0450dc
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-08-24 18:58:03 +0530

    24-08-2024

--- src/exam/prime.java
+++ src/exam/prime.java
@@ -1,5 +1,29 @@
 package exam;
 
 public class prime {
+    public static void  main(String []args)
+    {
+        int num = 0,sum=0;
 
+        for(int i=2;i<=50;i++)
+        {
+            num=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    num++;
+                }
+                if(num==2)
+                {
+                    System.out.println(i);
+                    sum=sum+i;
+                }
+            }
+
+
+        }
+
+        System.out.println("THE SUM OF PRIME NUMBERS FROM 1 TO N :"+sum);
+    }
 }
